db2101b871a04b3c1d367de901b3e1d01f71607a
Lucas Gomes De Araujo <lucas.araujo@g.globo>
2026-05-20 14:49:03 -0300

    problem vercel

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,8 @@
 
 import './App.css'
-import BananaPage from './Banana/bananaPage';
+import BananaPage from '../components/Banana/bananaPage';
+import Header from '../components/Header/header'
 // import ApplePage from './components/Apple/applePage'
-import Header from './Header/header'
 // import TangerinaPage from './components/Tangerina/tangerinaPage';
 function App() {
 
